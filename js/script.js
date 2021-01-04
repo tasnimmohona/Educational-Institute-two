6e9069f3100fcaf57b7472b8945c49c91618846d
Tasnim Mohona <67105895+tasnimmohona62@users.noreply.github.com>
2021-01-04 13:41:37 +0600

    edit

--- js/script.js
+++ js/script.js
@@ -1,62 +1,62 @@
 //preloader
-$(window).on("load", function() {
-    $("#preloader").fadeOut();
+$(window).on("load", function () {
+	$("#preloader").fadeOut();
 });
 
 //popup
-$(document).ready(function() {
-    setTimeout(function() {
-        $("#popUpmain").css('display', 'block');
-    }, 2000);
+$(document).ready(function () {
+	setTimeout(function () {
+		$("#popUpmain").css('display', 'block');
+	}, 2000);
 });
 
-$("#subscribeId").click(function() {
-    $("#popUpmain").css('display', 'none');
+$("#subscribeId").click(function () {
+	$("#popUpmain").css('display', 'none');
 });
 
 
 // sticky navbar on scroll down
-window.addEventListener("scroll", function() {
-    var header = document.querySelector(".navbar-dark");
-    header.classList.toggle("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function () {
+	var header = document.querySelector(".navbar-dark");
+	header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 
 // smooth scroll
 $("#nav").onePageNav();
 
-// keep reading
+//keep reading
 function showhide() {
 
-    $(".keepbuttonone").click(function() {
-        $(".showone").toggle();
-    });
-    $(".keepbuttontwo").click(function() {
-        $(".showtwo").toggle();
-    });
-    $(".keepbuttonthree").click(function() {
-        $(".showthree").toggle();
-    });
+	$(".keepbuttonone").click(function () {
+		$(".showone").toggle();
+	});
+	$(".keepbuttontwo").click(function () {
+		$(".showtwo").toggle();
+	});
+	$(".keepbuttonthree").click(function () {
+		$(".showthree").toggle();
+	});
 
-    $(".keepbuttonfour").click(function() {
-        $(".showfour").toggle();
-    });
+	$(".keepbuttonfour").click(function () {
+		$(".showfour").toggle();
+	});
 
-    $(".keepbuttonfive").click(function() {
-        $(".showfive").toggle();
-    });
+	$(".keepbuttonfive").click(function () {
+		$(".showfive").toggle();
+	});
 
-    $(".keepbuttonsix").click(function() {
-        $(".showsix").toggle();
-    });
+	$(".keepbuttonsix").click(function () {
+		$(".showsix").toggle();
+	});
 
-    $(".keepbuttonseven").click(function() {
-        $(".showseven").toggle();
-    });
+	$(".keepbuttonseven").click(function () {
+		$(".showseven").toggle();
+	});
 
-    $(".keepbuttoneight").click(function() {
-        $(".showeight").toggle();
-    });
+	$(".keepbuttoneight").click(function () {
+		$(".showeight").toggle();
+	});
 
 }
 showhide();
@@ -64,9 +64,9 @@ showhide();
 
 //Aos
 AOS.init({
-    delay: 400,
-    disable: function() {
-        var maxWidth = 1050;
-        return window.innerWidth < maxWidth;
-    }
+	delay: 400,
+	disable: function () {
+		var maxWidth = 1050;
+		return window.innerWidth < maxWidth;
+	}
 });
